da5a121bb0d04b366398dcac638c8f2e26bce5f8
Kirill Spesivtsev <kirill.spesivcev@innowise.com>
2026-05-07 20:04:47 +0200

    Set up Rabbitmq

--- DataIngestor/Program.cs
+++ DataIngestor/Program.cs
@@ -1,9 +1,27 @@
 
 
+using DataIngestor.Configuration;
+using MassTransit;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+builder.Services.AddMassTransit(x =>
+{
+    x.SetKebabCaseEndpointNameFormatter();
+    x.UsingRabbitMq((ctx, cfg) =>
+    {
+        var rabbitOptions = ctx.GetRequiredService<IOptions<RabbitMqOptions>>().Value;
+        cfg.Host(rabbitOptions.Host, "/", h =>
+        {
+            h.Username(rabbitOptions.Username);
+            h.Password(rabbitOptions.Password);
+        });
+    });
+});
 
 var app = builder.Build();
 
--- DataProcessor/DataProcessor.Api/Program.cs
+++ DataProcessor/DataProcessor.Api/Program.cs
@@ -1,5 +1,3 @@
-using DataProcessor.Domain.Entities;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
