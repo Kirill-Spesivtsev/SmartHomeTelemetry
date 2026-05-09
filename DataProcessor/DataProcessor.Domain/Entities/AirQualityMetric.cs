5df17611bcb4b10ba3a155e3c98e49f56ffa9e57
Kirill Spesivtsev <kirill.spesivcev@innowise.com>
2026-05-09 14:02:32 +0200

    Created domain entities

---
+++ DataProcessor/DataProcessor.Domain/Entities/AirQualityMetric.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataProcessor.Domain.Entities;
+
+public class AirQualityMetric : BaseMetric
+{
+    public int Co2 { get; set; }
+
+    public int Pm25 { get; set; }
+
+    public int Humidity { get; set; }
+}
---
+++ DataProcessor/DataProcessor.Domain/Entities/BaseMetric.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataProcessor.Domain.Entities;
+
+public class BaseMetric
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public long LocationId { get; set; }
+    public Location Location { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+}
