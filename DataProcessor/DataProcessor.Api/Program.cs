fe1722c5f64929cbe8d6b7baf65cf0205cd68fa6
Kirill Spesivtsev <kirill.spesivcev@innowise.com>
2026-05-09 14:46:05 +0200

    Set up postgres interaction

--- DataProcessor/DataProcessor.Api/Program.cs
+++ DataProcessor/DataProcessor.Api/Program.cs
@@ -1,11 +1,29 @@
+using DataProcessor.Api.Configuration;
+using DataProcessor.Api.Extensions;
+using DataProcessor.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using SmartHomeTelemetry.Shared.Contracts;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Host.UseSerilog((ctx, services, cfg) =>
+{
+    cfg.ReadFrom.Configuration(ctx.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext()
+        .WriteTo.Console();
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<TelemetryDbContext>(o =>
+{
+    var cs = builder.Configuration.GetConnectionString("Postgres");
+    o.UseNpgsql(cs);
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,7 +32,11 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.UseSerilogRequestLogging();
+
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 app.UseAuthorization();
 
