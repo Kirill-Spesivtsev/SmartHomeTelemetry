51ba5b4239f661e861baaae1502d9bdad1e90e2f
Kirill Spesivtsev <kirill.spesivcev@innowise.com>
2026-05-09 15:03:55 +0200

    Set up rabbitmq connection

--- DataProcessor/DataProcessor.Api/Program.cs
+++ DataProcessor/DataProcessor.Api/Program.cs
@@ -1,11 +1,13 @@
 using DataProcessor.Api.Configuration;
 using DataProcessor.Api.Extensions;
 using DataProcessor.Infrastructure;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 using SmartHomeTelemetry.Shared.Contracts;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<RabbitMqOptions>(builder.Configuration.GetSection(RabbitMqOptions.SectionName));
 
 builder.Host.UseSerilog((ctx, services, cfg) =>
 {
@@ -24,13 +26,31 @@ builder.Services.AddDbContext<TelemetryDbContext>(o =>
     var cs = builder.Configuration.GetConnectionString("Postgres");
     o.UseNpgsql(cs);
 });
-var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+builder.Services.AddMassTransit(x =>
+{
+    x.SetKebabCaseEndpointNameFormatter();
+    x.AddConsumer<TelemetryEventConsumer>();
+
+    x.UsingRabbitMq((ctx, cfg) =>
+    {
+        var rabbitOptions = ctx.GetRequiredService<Microsoft.Extensions.Options.IOptions<RabbitMqOptions>>().Value;
+        cfg.Host(rabbitOptions.Host, "/", h =>
+        {
+            h.Username(rabbitOptions.Username);
+            h.Password(rabbitOptions.Password);
+        });
+
+        cfg.ReceiveEndpoint(TelemetryTopics.TelemetryQueue, e =>
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-}
+            e.UseMessageRetry(r => r.Interval(3, TimeSpan.FromSeconds(2)));
+        });
+    });
+});
+
+var app = builder.Build();
 
 app.UseSerilogRequestLogging();
 
