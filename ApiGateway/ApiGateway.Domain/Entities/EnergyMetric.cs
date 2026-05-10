844371a77eb5ef6f9c9684e70667ceb80db36438
Kirill Spesivtsev <kirill.spesivcev@innowise.com>
2026-05-10 14:23:18 +0200

    Added domain entities

---
+++ ApiGateway/ApiGateway.Domain/Entities/EnergyMetric.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiGateway.Domain.Entities;
+
+public class EnergyMetric : BaseMetric
+{
+    public float Energy { get; set; }
+}
